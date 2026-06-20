542e95adefe8f2643c1e17922299eb061de20dff
Khanh Nguyen <kykhanhnguyenmh@gmail.com>
2026-06-20 21:37:13 +0700

    Complete Bank Management System v1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const string CCCD = "094208004009";
 const string PASSWORD = "123aloalo";
 bool login = false;
-long sodu = 5000000000;
+long long sodu = 5000000000;
 
 int main() {
 	while(true) {
@@ -44,7 +44,7 @@ int main() {
 			}
 			case 2: {
 				if (login==false) {
-					cout << "Vui long dang nhap truoc!" << endl;
+					cout << "Vui long long dang nhap truoc!" << endl;
 					break;
 				}
 				cout << "So du hien tai: " << sodu << " dong." << endl;
@@ -52,13 +52,17 @@ int main() {
 			}
 			case 3: {
 				if (login==false) {
-					cout << "Vui long dang nhap truoc!" << endl;
+					cout << "Vui long long dang nhap truoc!" << endl;
 					break;
 				}
 				cout << "===== BANK SYSTEM =====" << endl;
-				cout << "Nhap so tien nap: ";
-				long sotiennap;
+				cout << "Nhap so tien nap (don vi dong): ";
+				long long sotiennap;
 				cin >> sotiennap;
+				if (sotiennap < 2000) {
+					cout << "So tien nap khong hop le!" << endl;
+					break;
+				}
 				cout << "Ban co chac chan nap so tien " << sotiennap << " vao tai khoan khong?" << endl;
 				cout << "Y/N: ";
 				char recheck;
@@ -77,18 +81,51 @@ int main() {
 				}
 				break;
 			}
-			case 4:
+			case 4: {
 				if (login==false) {
 					cout << "Vui long dang nhap truoc!" << endl;
 					break;
 				}
+				cout << "===== BANK SYSTEM =====" << endl;
+				cout << "Nhap so tien can rut (don vi dong): ";
+				long long sotienrut;
+				cin >> sotienrut;
+				if (sodu<sotienrut) {
+					cout << "So du hien tai khong du!" << endl;
+					break;
+				}
+				else if (sotienrut < 2000) {
+					cout << "So tien rut khong hop le!" << endl;
+					break;
+				}
+				cout << "Ban co chac chan rut so tien " << sotienrut << " dong" << "khoi so du tai khoan hien tai khong?" << endl;
+				cout << "Y/N: ";
+				char recheck;
+				cin >> recheck;
+				switch(recheck) {
+					case 'y':
+					case 'Y':
+						sodu -= sotienrut;
+						sotienrut = 0;
+						cout << "So du hien tai la: " << sodu << " dong" << endl;
+						break;
 
+					case 'n':
+					case 'N':
+					default:
+						sotienrut = 0;
+						break;
+				}
+				break;
+			}
 			case 5:
 				if (login==false) {
-					cout << "Vui long dang nhap truoc!" << endl;
+					cout << "Vui long long dang nhap truoc!" << endl;
 					break;
 				}
-
+				login = false;
+				cout << "Dang xuat thanh cong!" << endl;
+				break;
 			default:
 				cout << "Lua chon khong hop le!" << endl;
 				break;
